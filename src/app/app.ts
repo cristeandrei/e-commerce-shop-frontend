16bb6ab36b356163e77e9ac268675d59a56b1815
acriste <cristeandreiflorin@gmail.com>
2025-11-26 17:05:20 +0000

    updated app

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('e-commerce-shop-frontend');
+  protected readonly title = signal('e-commerce');
 }
